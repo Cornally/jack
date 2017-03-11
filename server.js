3463414d1766152ef1db92a5ed832c7ec0cbabfe
Chris Connelly <chrisconnelly@gmail.com>
2017-03-11 14:44:44 -0500

    Output compiled lib to  folder and  folder.

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
 
 // Serve compiled docs
-app.use('/dist', express.static(__dirname + '/dist'));
+app.use('/', express.static(__dirname + '/docs'));
 
 // Viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/docs/index.html'));
 });
 
 app.listen(8080);
