2d5b48067491186b4d6d0f8dcaed2998461705cd
Chris Connelly <chrisconnelly@gmail.com>
2017-04-03 22:59:45 -0400

    Moved Jekyll output folder to  for our github page

--- copy.js
+++ copy.js
@@ -1,5 +1,5 @@
 var copy = require('copy');
-copy('src/**/*.*', 'docs/diane-theme/_sass', function(err, files) {
+copy('src/**/*.*', 'docs-src/diane-theme/_sass', function(err, files) {
   if (err) throw err;
   // `files` is an array of the files that were copied 
 });
